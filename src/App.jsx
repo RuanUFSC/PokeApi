3938b2dfebff0c782b783c35a511363430049076
RuanUFSC <ruaanlopes@hotmail.com>
2023-05-09 18:27:48 -0300

    Listagem dos pokemons

--- src/App.jsx
+++ src/App.jsx
@@ -3,44 +3,25 @@ import { useState } from 'react'
 
 function PokemonSearch() {
 
-  const [pikachu, setPikachu] = useState(null);
-  const [charizard, setCharizard] = useState(null);
+  const [pokemons, setPokemons] = useState([]);
 
-  const buscaPokemons = async () => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/pikachu/`)
-    .then((response) => response.json())
-    .then((data) => {
-      setPikachu(data)
-    })
-
-    fetch(`https://pokeapi.co/api/v2/pokemon/charizard/`)
-    .then((response) => response.json())
-    .then((data) => {
-      setCharizard(data)
-    })
+  async function fetchPokemons() {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100');
+    const data = await response.json();
+    setPokemons(data.results);
+    console.log(data.results);
   }
 
-  buscaPokemons();
+  fetchPokemons();
+
 
   return (
     <>
-      {/* <button onClick={buscaPokemons}>Buscar Pokemons</button> */}
-      <div>
-          <div>
-            <h2>{pikachu?.name}</h2>
-            <img src={pikachu?.sprites.front_default} alt={pikachu?.name} />
-            <p>Altura: {pikachu?.height}</p>
-            <p>Peso: {pikachu?.weight}</p>
-          </div>
-      </div>
-      <div>
-          <div>
-            <h2>{charizard?.name}</h2>
-            <img src={charizard?.sprites.front_default} alt={charizard?.name} />
-            <p>Altura: {charizard?.height}</p>
-            <p>Peso: {charizard?.weight}</p>
-          </div>
-      </div>
+        {pokemons.map(pokemon => (
+            <div key={pokemon?.name}>
+              <h4>{pokemon?.name}</h4>
+            </div>
+        ))}
     </>
   );
 }
