e06dc4a32e5168815646b693db3e4d66ffb1f772
RuanUFSC <ruaanlopes@hotmail.com>
2023-05-15 23:49:11 -0300

    Lista de tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,82 @@
-import './App.css'
 import { useState } from 'react'
-
-function PokemonSearch() {
-
-  const [pokemons, setPokemons] = useState([]);
-
-  async function fetchPokemons() {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100');
-    const data = await response.json();
-    setPokemons(data.results);
-    console.log(data.results);
-  }
-
-  fetchPokemons();
-
-
-  return (
-    <>
-        {pokemons.map(pokemon => (
-            <div key={pokemon?.name}>
-              <h4>{pokemon?.name}</h4>
-            </div>
-        ))}
-    </>
-  );
-}
+import './App.css'
 
 function App() {
+
+  const [tarefas, setTarefas] = useState([]);
+  const [filtro, setFiltro] = useState('todas');
+  const [novaTarefa, setNovaTarefa] = useState('');
+
+  const mudarFiltro = (event) => {
+    setFiltro(event.target.value);
+  };
+
+  const adicionarTarefa = () => {
+    if (novaTarefa.trim() !== '') {
+      const newTarefa = {
+        id: tarefas.length + 1,
+        title: novaTarefa,
+        status: 'pendente'
+      };
+      setTarefas([...tarefas, newTarefa]);
+      setNovaTarefa('');
+    }
+  };
+
+  const tarefasFiltradas = tarefas.filter(tarefa => {
+    if (filtro === 'todas') {
+      return true;
+    } else {
+      return tarefa.status === filtro;
+    }
+  });
+
+  const completarTarefa = (tarefaId) => {
+    const updatedTarefas = tarefas.map(tarefa => {
+      if (tarefa.id === tarefaId) {
+        return { ...tarefa, status: tarefa.status == 'completa' ? 'pendente' : 'completa' };
+      }
+      return tarefa;
+    });
+    setTarefas(updatedTarefas);
+  };
+
   return (
     <>
-      <h1>Consulta de Pokemons</h1>
-      <PokemonSearch />   
+      <h2>Tarefas</h2>
+      <h3>Adicione uma tarefa</h3>  
+      {/* value vai ajudar a limpar input */}
+      <input
+        type="text"
+        value={novaTarefa}
+        onChange={(event) => setNovaTarefa(event.target.value)}
+        placeholder="Adicione uma tarefa"
+      />
+      <button onClick={adicionarTarefa}>Adicionar</button>
+      <h3>Lista de tarefas</h3>
+      <div>
+        Filtro: 
+        <select id="filtro" value={filtro} onChange={mudarFiltro}>
+          <option value="todas">Todas</option>
+          <option value="completa">Completas</option>
+          <option value="pendente">Pendentes</option>
+        </select>
+      </div>
+
+      <ul>
+        {tarefasFiltradas.map(tarefa => (
+          <li key={tarefa.id}>
+              {tarefa.title}            
+              {tarefa.status == 'completa' ? 
+                (<button onClick={() => completarTarefa(tarefa.id)} style={{ backgroundColor: 'green' }}>
+                Completa</button>)
+                :                
+                (<button onClick={() => completarTarefa(tarefa.id)} style={{ backgroundColor: 'red' }}>
+                Pendente</button>)
+              }            
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
